56727f60cd99b224fb217ccc6a7e2ad80c0234ed
Chase <sde.chase.rogers@gmail.com>
2020-11-15 01:52:07 -0700

    stage for deployment

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const path = require("path")
+const port = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.json());
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 //Connect to DB
-mongoose.connect('mongodb://localhost:27017/bountiesdb', 
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(`${process.env.DB_SERVER}`, 
+// mongoose.connect(`mongodb://localhost:27017/bountiesdb`, 
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -25,7 +31,11 @@ app.use((err, req, res, next) => {
     return res.send({Error: err.message})
 })
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 // 1: port   2: callback function
-app.listen(8000, () => {
-    console.log("The server is running on port 8000");
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
 });
